f68659e229ed56a82f10e610f08a08c36491cb68
Serhii Cherneha <chernegasergiy3@gmail.com>
2025-12-30 17:35:39 +0200

    refactor: Update App.vue to use global layout components

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import TheNavBar from './components/TheNavBar.vue';
+import TheFooter from './components/TheFooter.vue';
+</script>
+
+<template>
+  <TheNavBar />
+  <RouterView />
+  <TheFooter />
+</template>
+
+<style scoped>
+/* No specific scoped styles needed for App.vue if components handle their own styling */
+</style>
