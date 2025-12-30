7eeaca25b12f9da95cb4d86b33360bd4e3817ba1
Serhii Cherneha <chernegasergiy3@gmail.com>
2025-12-30 19:28:09 +0200

    fix(router): Correct createWebHashHistory usage

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import HomePage from '../views/HomePage.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
