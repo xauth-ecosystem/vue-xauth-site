a7cf287c16a6bed4544eb414d115b8f60dc4f05a
Serhii Cherneha <chernegasergiy3@gmail.com>
2025-12-30 20:53:09 +0200

    fix(icons): Integrate Font Awesome directly in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 import TheNavBar from "@/components/TheNavBar";
 import TheFooter from "@/components/TheFooter";
